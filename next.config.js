5ee006b4dcaba297d79abebadae1d13b0c7ee406
emerele <amirali.behboudi@gmail.com>
2024-10-26 22:47:20 +0400

    Update Next.js configuration for static export

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export", // Add this for static site generation
+};
+
+module.exports = nextConfig;
